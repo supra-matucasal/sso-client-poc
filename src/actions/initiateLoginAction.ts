3645a615c5d0fdded10ca5b89dd21a3708482322
supra-matucasal <matias.casal@yahoo.com.ar>
2024-05-22 14:18:09 +0200

    Add action to get client url

---
+++ src/actions/initiateLoginAction.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { randomBytes } from 'crypto';
+
+export async function initiateLoginAction() {
+  const redirectUrl = process.env.AUTH_REDIRECT_URL || 'http://localhost:4000/api/callback';
+  const secretToken = process.env.TOKEN_EXTERNAL_APPS || 'token';
+  const authSSOServer = process.env.AUTH_SSO_SERVER || 'http://localhost:3000';
+
+  const state = randomBytes(16).toString('hex');
+
+  // Fetch to SSO-Server auth/generate-login -POST function
+  try {
+    const response = await fetch(`${authSSOServer}/api/auth/generate-login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "origin": "http://localhost:4000"
+      },
+      body: JSON.stringify({ redirect: redirectUrl, token: secretToken, state: state }),
+    });
+
+    if (response.status === 200) {
+      const data = await response.json();
+      return data.redirectTo;
+    } else {
+      throw new Error('Failed to generate login');
+    }
+  } catch (error) {
+    console.error('Error generating login', error);
+    throw error;
+  }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,40 +1,20 @@
 'use client';
 import { useEffect } from 'react';
 import { randomBytes } from 'crypto';
+import { initiateLoginAction } from '@/actions/initiateLoginAction';
+import { redirect } from 'next/navigation';
+
 
 
 export default function Login() {
 
-
-
-
-  useEffect(() => {
+useEffect(() => {
     const initiateLogin = async () => {
-      console.log('Initiating login...')
-      const redirectUrl = process.env.AUTH_REDIRECT_URL || 'http://localhost:4000/api/callback';
-      const secretToken = process.env.TOKEN_EXTERNAL_APPS || 'token';
-      const authSSOServer = process.env.AUTH_SSO_SERVER || 'http://localhost:3000';
-
-      const state = randomBytes(16).toString('hex');
-
       //Fetch to SSO-Server auth/generate-login -POST function
       try {
-        const response = await fetch(`${authSSOServer}/api/auth/generate-login`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ redirect: redirectUrl, token: secretToken, state: state })
-        });
+        const redirectTo = await initiateLoginAction();
+        window.location.href = redirectTo;
 
-        console.log('response', response)
-
-        if (response.status === 200) {
-          const data = await response.json();
-          console.log('data: ', data)
-          console.log('data.redirectUr: ', data.redirectTo)
-          window.location.href = data.redirectTo;
-        }
       } catch (error) {
         console.log('Error generating login', error);
       }
@@ -43,4 +23,5 @@ export default function Login() {
   }, []);
 
   return <div>Redirecting to login...</div>;
+
 }
